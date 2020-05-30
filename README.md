98254035c56e13c91b0970f22fdae6e69d24ec70
Dheeraj <Dheeraj160199@gmail.com>
2020-05-30 16:37:48 +0530

    final edit

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Mysql_to_excel
     Exporting mysql data into excel sheet
     
+    LIBRARIES USED:
+        Mysql  
+        openpyxl
+
     INPUTS:
         database name
         table name
     
     OUTPUT:
         exporting all data from database to excel_sheet with same name as table name
-    
--- main.py
+++ main.py
@@ -1,23 +1,7 @@
 import mysql.connector as mysql
 from openpyxl import Workbook
 
-
-def data_prep(structure,data):
-    data.insert(0,[i[0] for i in structure])
-    return data
-
-def push_data(data,tb_name):
-    try:    
-        wb = Workbook()  
-        sheet = wb.active      
-        for i in data:  
-            print(i)
-            sheet.append(i)  
-        wb.save(f"{tb_name}.xlsx")  
-    except Exception as err:
-        print(err)
-        exit()
-
+#initializing connection to Mysql host
 def sql_init(host,user,password,db):
     try:
         conn=mysql.connect(host=host,user=user,passwd=password,database=db)
@@ -26,35 +10,41 @@ def sql_init(host,user,password,db):
         print("error occured --> ",err)
         exit()
 
-
+#function to execute sql queries 
 def sql_query(conn,query):
     cursor=conn.cursor()
     cursor.execute(query)
     records=cursor.fetchall()
     return records
 
+#inserting column_names into data
+def data_prep(structure,data):
+    data.insert(0,[i[0] for i in structure])
+    return data
 
-db_name=input('## Enter database name : ')
-conn=sql_init('localhost','root','',db_name)
-tb_name=input('## Enter table name : ')
-structure=sql_query(conn,f'show columns from {tb_name}')
-#print(structure)
-data=sql_query(conn,'select * from blending_s')
-#print(data)
-data=data_prep(structure,data)
+#Exporting excel_sheet
+def push_data(data,tb_name):
+    try:    
+        wb = Workbook()  
+        sheet = wb.active      
+        for i in data:  
+            print(i)
+            sheet.append(i)  
+        wb.save(f"{tb_name}.xlsx")
+        print('## Export Finished ##')  
+    except Exception as err:
+        print(err)
+        exit()
 
-push_data(data,tb_name)
+#main function
+def main():
+    db_name=input('## Enter database name : ')
+    conn=sql_init('localhost','root','',db_name)
+    tb_name=input('## Enter table name : ')
+    structure=sql_query(conn,f'show columns from {tb_name}')
+    data=sql_query(conn,f'select * from {tb_name}')
+    data=data_prep(structure,data)
+    push_data(data,tb_name)
 
-#sql_query(conn,'select * from datarefresh')
-
-
-"""
-db_name=input("Enter Database Name")
-table_name=input("Enter Table Name")
-file_path=input("Enter path where excel sheet is to be save")
-
-"""
-
-
-#get data from database
-#export it into excel file
+if __name__ == "__main__":
+    main()
